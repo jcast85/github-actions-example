a41c82b31fbe356b3ec69c24875f71a5930144fa
juri.castellani <juri.castellani@hotmail.com>
2025-05-02 02:54:48 +0200

    A TODO has been added.

--- backend/src/main/java/com/jc/crud/example/config/WebConfig.java
+++ backend/src/main/java/com/jc/crud/example/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
-      .allowedOrigins("http://localhost:4300")  // Your Angular app's origin
+      .allowedOrigins("http://localhost:4300")  // TODO use environment variable
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true);
