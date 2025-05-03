312841f9ae25721609a977af18ed116796365f25
juri.castellani <juri.castellani@hotmail.com>
2025-05-04 01:06:46 +0200

    frontend image has been improved

--- backend/src/main/java/com/jc/crud/example/config/WebConfig.java
+++ backend/src/main/java/com/jc/crud/example/config/WebConfig.java
@@ -1,16 +1,19 @@
 package com.jc.crud.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  @Value("${frontend.base.url}")
+  private String frontendBaseUrl;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
-      .allowedOrigins("http://localhost:4300")  // TODO use environment variable
+      .allowedOrigins(frontendBaseUrl)
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true);
