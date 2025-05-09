9a7ca02fc6e299810f068824448ca26ede8de9cd
juri.castellani <juri.castellani@hotmail.com>
2025-05-09 23:23:02 +0200

    frontend image has been improved

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,26 +2,35 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
   });
 
+  afterEach(() => {
+    if (fixture) {
+      fixture.destroy();
+      fixture = null;
+    }
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-first-project' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have the 'MyFirstProject' title`, () => {
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('my-first-project');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-first-project');
